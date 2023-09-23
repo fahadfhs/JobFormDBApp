8402de5763a2097117df871762fb4777ca17e0a7
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-09-23 12:08:28 -0400

    created the home page routing and defined a function for rendering the template called index.html. also created the index.html file and its basis structure

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# -------------------------------------------------------------------------------------#
 # ----We handle url requests or https requests in between app and app.run----
 # if you load one page with your enter key, you are making an http request
 # with python you receive THAT request then allocate that request to particular HTML page!!
+# -------------------------------------------------------------------------------------#
+
+# home page is handled by ("/") request
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 app.run(debug=True, port=5002)
