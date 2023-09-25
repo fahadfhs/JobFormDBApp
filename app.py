9e7dac0e6b7c1511d1877334b7ab338c66a699fa
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-09-25 10:11:23 -0400

    imported datetime, changes the date in the index  function to strip the datetime of date that we created level code above as it was not recognized in the process when ran. Created form instance with the variables and the added the session to the form and then committed to store it to the database.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -35,7 +36,15 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        # add the form to the session
+        db.session.add(form)
+        # stores the data to the database
+        db.session.commit()
 
     return render_template("index.html")
 
